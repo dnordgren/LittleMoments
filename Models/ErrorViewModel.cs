5e9950978aef241b0396512b7d9099e8632ac609
Derek Nordgren <derek@dereknordgren.com>
2024-11-05 12:07:16 -0500

    feat: Scaffold .NET MVC project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LittleMoments.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
