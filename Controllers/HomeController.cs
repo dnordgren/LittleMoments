193109aae9dadaca8c938f25a40add7a9ef1a957
Derek Nordgren <dnordgren@users.noreply.github.com>
2024-11-06 02:12:16 +0000

    feat: Set up Hotwire with ASP.NET in toy app. (#1)
    * Install Turbo JS for HTML over the wire
    * Note: Form doesn't clear on submission. ASP bug? Firefox bug? My bug?

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,23 +4,38 @@ using LittleMoments.Models;
 
 namespace LittleMoments.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<HomeController> _logger = logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    private static readonly List<TurboMessage> Messages = [];
 
     public IActionResult Index()
     {
-        return View();
+        var model = new ChatViewModel
+        {
+            Messages = Messages
+        };
+        return View(model);
     }
 
-    public IActionResult Privacy()
+    [HttpPost]
+    public IActionResult PostMessage(string text)
     {
-        return View();
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return BadRequest();
+        }
+        
+        var newTurboMessage = new TurboMessage
+        {
+            Text = text,
+            Timestamp = DateTime.Now,
+        };
+
+        Messages.Add(newTurboMessage);
+
+        return PartialView("_MessageListPartial", Messages);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -28,4 +43,15 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public class TurboMessage
+{
+    public required string Text { get; set; }
+    public DateTime Timestamp { get; set; }
+}
+
+public class ChatViewModel
+{
+    public required List<TurboMessage> Messages { get; set; }
 }
